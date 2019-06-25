6d7c434104a7f85d332b8c0855110255710c66c8
kireeti <kireeti@accendo.com.my>
2019-06-25 18:34:24 +0800

    work in progress

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,19 +4,55 @@ import {useNavigation} from 'react-navigation-hooks';
 import AsyncStorage from '@react-native-community/async-storage';
 import {homeStyles} from "./homeCss";
 import {LOGIN} from "../../navigation/navigationConstants";
+import {apiEndPoint, tokenName} from "../../constants";
+import {Card} from "react-native-elements";
+import moment from "moment";
+import {commonStyles} from "../../commonStyles";
+import * as _ from 'lodash';
 
 export const HomeScreen = () => {
-    const { navigate } = useNavigation();
+    const {navigate} = useNavigation();
+    const [games, setGames] = React.useState([]);
 
     const logout = () => {
         AsyncStorage.removeItem('playToken');
         navigate(LOGIN);
     };
 
+    const getGames = () => {
+        AsyncStorage.getItem(tokenName).then(token => {
+            fetch(`${apiEndPoint}/game/get?token=${token}`).then(res => res.json()).then(res => {
+                setGames(_.groupBy(res.data, 'sportName'));
+            })
+        })
+    };
+
+    const Capitalize = (str) => {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    };
+
+    React.useEffect(() => {
+        getGames();
+    });
+
     return (<View>
         <Text style={homeStyles.heading}>Home Page</Text>
+        {Object.keys(games).map((key) => {
+            const game = games[key];
+            return (<View key={key}>
+                <Text style={[commonStyles.heading]}>{Capitalize(key)}</Text>
+                {game.map(item => {
+                    return (<Card key={item._id}>
+                        <Text style={commonStyles.fwbold}>Date: {moment(item.date).format('LL HH:mm')}</Text>
+                        <Text>Venue: {item.venue}</Text>
+                        <Text>Hosted by: {item.hostName}</Text>
+                    </Card>)
+                })}
+            </View>)
+        })}
+
         <Button title={'Logout'} color={'red'}
-            onPress={logout}
+                onPress={logout}
         />
     </View>)
 };
